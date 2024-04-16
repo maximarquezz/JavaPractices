a848055d3b7ab95432b89a2fc14e971cf85ae486
maximarquezz <marquezmaxi122@gmail.com>
2024-04-16 11:41:02 -0300

    Probando desarrollar en Java a través de VSC

--- src/cursojava/ArrayBidimensional.java
+++ src/cursojava/ArrayBidimensional.java
@@ -1,13 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package cursojava;
 
-/**
- *
- * @author FM 96.1
- */
 public class ArrayBidimensional {
+    
+    public static void main(String args[]){
+        
+        //Array unidimensional
+        int[] num1 = new int[2];
+        num1[0] = 12;
+        num1[1] = 65;
+        
+        //Array bidimensional
+        int[][] num2 = new int[2][2];
+        num2[0][0] = 51;
+        num2[0][1] = 89;
+        
+        num2[1][0] = 60;
+        num2[1][1] = 75;
+        
+        //Y así sucesivamente...
+        
+        for (int i = 0; i < 2; i++){
+            for (int j = 0; j < 2; j++){
+                System.out.println(num2[i][j]);
+            }
+        }
+        
+    }
     
 }
